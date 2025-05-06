e5b076ea761847f25ce58f99ec3c310970016a03
PantelisBDEV <pantelis0159@gmail.com>
2025-05-06 21:44:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,10 @@ import asyncio
 from PIL import Image, ImageDraw, ImageFont
 from discord.ext import commands, tasks
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
-load_dotenv()  # Φορτώνει μεταβλητές από το .env αρχείο
+#load_dotenv()  # Φορτώνει μεταβλητές από το .env αρχείο
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
